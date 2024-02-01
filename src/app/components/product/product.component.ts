393de846734161331e4f930df7dcc861d0aa84bb
Nikole Caballero Garrido <nikole@Nikoles-MacBook-Pro-15.local>
2024-01-31 21:50:52 -0400

    Actualizado con la clase del miercoles 31 enero

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,23 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { ImgComponent } from "../img/img.component";
+import { CommonModule } from '@angular/common';
+import { time } from 'console';
+import { ReversePipe } from '../../pipes/reverse.pipe';
+import { TimeAgoPipe } from '../../pipes/time-ago.pipe';
+import { HighlightDirective } from '../../directives/highlight.directive';
 
 @Component({
     selector: 'app-product',
     standalone: true,
     templateUrl: './product.component.html',
     styleUrl: './product.component.scss',
-    imports: [ImgComponent]
+    imports: [ImgComponent, CommonModule, ReversePipe, TimeAgoPipe, HighlightDirective]
 })
 export class ProductComponent {
+  now: Date = new Date();
+  pastDate: Date = new Date(2023,12,1);
+
   @Input() product : Product = {
     id: 0,
     title: "",
